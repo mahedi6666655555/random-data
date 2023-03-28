4566d55a4e8711381e9b420b9374452c4784bcfc
mahedi6666655555 <mahedinir34678@gmail.com>
2023-03-29 01:54:25 +0600

    relode btn is added new

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-const Card = ({api}) => {
-    console.log(api);
-  
+import React, { useState } from "react";
+const Card = ({ api }) => {
+  // let [relode,setRelode]=useState([])
 
-  console.log(api);
-
+  let relode1 = () => {
+    location.reload();
+  };
   return (
     <div>
       <h1>{api.id}</h1>
       <h1>{api.brand}</h1>
       <h1>{api.equipment}</h1>
       <h1>{api.uid}</h1>
+      <button onClick={relode1}>relode</button>
     </div>
   );
 };
